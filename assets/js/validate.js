e36beeafbe2d31aac0e47c2fde4429596a6200db
Bankole Esan <techybanky@gmail.com>
2019-06-19 12:49:46 +0100

    Updates

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,6 +1,26 @@
 $("#ride-next").click(function(e) {
   e.preventDefault();
-  $("#1").addClass("show-out-left");
-  $("#2").addClass("show-in");
-  $("#2").removeClass("show-out-right");
+  let number = e.target.getAttribute('data-to');
+  let state = parseInt(number, 10);
+  let nextState = state + 1;
+  console.log(`${state} ${nextState}`);
+  $(`#${state}`).addClass("show-out-left");
+  $(`#${nextState}`).addClass("show-in");
+  $(`#${nextState}`).removeClass("show-out-right");
+  e.target.setAttribute('data-to', nextState);
+  $('#ride-back').attr('data-to', state);
+});
+
+$("#ride-back").click(function(e) {
+  e.preventDefault();
+  let number = e.target.getAttribute('data-to');
+  let prevState = parseInt(number, 10);
+  let currState = prevState + 1;
+  let nextBack = prevState - 1;
+  console.log(`${currState} ${prevState}`);
+  $(`#${currState}`).addClass("show-out-right");
+  $(`#${prevState}`).addClass("show-in");
+  $(`#${prevState}`).removeClass("show-out-left");
+  e.target.setAttribute('data-to', nextBack);
+  $('#ride-next').attr('data-to', prevState);
 });
