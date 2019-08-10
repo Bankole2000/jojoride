f2dfdc2bc8efb14fc66e7206e9de80c24ca8c4bc
Bankole <techybanky@gmail.com>
2019-08-10 12:07:08 +0100

    update

--- dist/assets/js/loggedIn.js
+++ dist/assets/js/loggedIn.js
@@ -7,8 +7,11 @@ const checkIfLoggedIn = () => {
   // get details from local storage
   const email = localStorage.getItem('jojoemail');
   const pass = localStorage.getItem('jojopass');
-  email && pass ? M.toast({html: `Welcome ${email}`, classes: "success", displayLength: 1000}) : alert("couldn't get to storage");
+  const isLoggedIn = email && pass ? true : false;
+  email && pass ? M.toast({html: `Welcome ${email}`, classes: "success", displayLength: 1000}) : M.toast({html: `Not Logged In`, classes: "error", completeCallback: () => { window.location.replace("../index.html"); }, displayLength: 1000});
+
   // Compare with session Data via Ajax request
+  return isLoggedIn;
 }
 
 checkIfLoggedIn();
