a9e11d8ed012c08bd930df88ce3b67a4385a274b
Bankole Esan <techybanky@gmail.com>
2019-08-15 22:27:57 +0100

    signup

--- dist/assets/scripts/signup.php
+++ dist/assets/scripts/signup.php
@@ -5,7 +5,10 @@
 // TODO: Check if User is logged in 
 // FIXME: 
 
-session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once '../vendor/autoload.php';
+
 
   if(isset($_POST["action"]))
   {
@@ -32,10 +35,55 @@ session_start();
       $result1 = $db->query($signup);
       $usermail = $_POST['email'];
       $username = $_POST['firstname'];
-      $password = $dfltpassword ;
-      $bizname = "Jojoride";
-      }
-    }
+      $userusername = $_POST['username'];
+      $code = $_POST['code'];
+      $password = "Your Chosen Password" ;
+      $bizname = "Jojoride"; 
 
-  }
+      include_once "../vendor/phpmailer/phpmailer/src/PHPMailer.php";
+		  $mail = new PHPMailer();
+		
+  		$mail->Host = 'smtp.gmail.com'; 
+		
+	  	$mail->Username = 'techybanky@gmail.com';                 // SMTP username
+    	$mail->Password = '1.41421356237';                           // SMTP password
+		  $mail->SMTPSecure = 'ssl';     
+    	$mail->Port = 465;
+    	$mail->setFrom('no-reply@jojoride.com', 'Jojoride');
+		  $mail->addAddress($usermail, $username);
+		  $mail->addAddress('techybanky@gmail.com'); // Admin Mail CC
+			
+		  // CONTENT 
+		
+		  $mail->isHTML(true);                                  // Set email format to HTML
+      $mail->Subject = 'Jojoride Verification Code';
+      $mail->Body = 'Welcome to '.$bizname.'. 
+      Hello '.$username.'. Welcome to '.$bizname.' and Congratulations on Signing up. Here are your Login Details: 
+      Username: '.$userusername.'. 
+      Email:  '.$usermail.'. 
+      Password: '.$password.'.
+      Verification Code: '.$code.' 
+      Please Enter the verification code to verify your email. Congratulations again and Welcome to '.$bizname.'. 
+      
+      You got this mail because we think you tried to sign up on www.'.$bizname.'.com. If you think we made a mistake, just ignore this message.';
+      
+      $mail->AltBody = 'Welcome to '.$bizname.'. 
+		  Hello '.$username.'. Welcome to '.$bizname.' and Congratulations on Signing up. Here are your Login Details: 
+		  Username: '.$userusername.'. 
+		  Email:  '.$usermail.'. 
+      Password: '.$password.'.
+      Verification Code: '.$code.' 
+		  Please Enter the verification code to verify your email. Congratulations again and Welcome to '.$bizname.'. 
+		
+		  You got this mail because we think you tried to sign up on www.'.$bizname.'.com. If you think we made a mistake, just ignore this message.';
+
+      $mail->send();
+
+      echo "successful"; 
+
+      }else{
+      echo "failed";
+      }
+     }
+    };
 ?>
